a9dd52383519438cc38baf4653beeec8f9e45a2c
cedric.farinazzo <cedric.farinazzo@epita.fr>
2019-07-03 00:22:39 +0200

    password: add database method
    Signed-off-by: cedric.farinazzo <cedric.farinazzo@epita.fr>

--- password/db/database.py
+++ password/db/database.py
@@ -1,4 +1,30 @@
+from .config import Config
+from .entry import Entry
+
+
 class Database:
 
-    def __init__(self):
+    def __init__(self, conf):
+        self.conf = conf
+        self.__data = []
+
+    def register(self, account, master_key):
+        pass
+
+    def retrieve(self, account_id, master_key):
+        pass
+
+    def delete(self, account_id, master_key):
+        pass
+
+    def update(self, account_id, data, master_key):
+        pass
+
+    def list(self):
+        pass
+
+    def load(self, master_key):
+        pass
+
+    def save(self):
         pass
